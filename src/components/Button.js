3d386bc05234c25cb4af494ca47d4ef753898cf5
mcken-vince <mcken.vince@gmail.com>
2021-08-31 14:11:56 -0600

    refactor Button.js to use classNames instead of if statements

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,17 +1,13 @@
 import React from "react";
+import classNames from 'classnames';
 
 import "components/Button.scss";
-import { action } from "@storybook/addon-actions/dist/preview";
 
 export default function Button(props) {
-  let buttonClass = "button";
-
-  if (props.confirm) {
-    buttonClass += " button--confirm";
-  }
-  if (props.danger) {
-    buttonClass += " button--danger";
-  }
+  const buttonClass = classNames("button", {
+    'button--confirm': props.confirm, 
+    'button--danger': props.danger
+  });
 
    return <button 
             disabled={props.disabled} 
