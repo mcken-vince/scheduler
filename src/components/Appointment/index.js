f2cc32f96d442d62e6ec5496e6db01505cdb8579
mcken-vince <mcken.vince@gmail.com>
2021-09-01 15:34:53 -0600

    import Status component into Appointment index.js

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -4,6 +4,7 @@ import Header from './Header';
 import Show from './Show';
 import Confirm from './Confirm';
 import Empty from './Empty';
+import Status from './Status';
 import Error from './Error';
 
 const Appointment = (props) => {
