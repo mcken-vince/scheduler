bb40142cb5cd127312e5f3786adee4b5d87c0943
mcken-vince <mcken.vince@gmail.com>
2021-09-01 15:20:52 -0600

    create Appointment Status component

--- stories/index.js
+++ stories/index.js
@@ -14,6 +14,9 @@ import InterviewerList from "components/InterviewerList";
 import Appointment from "components/Appointment/index";
 import Header from "components/Appointment/Header";
 import Empty from "components/Appointment/Empty";
+import Show from "components/Appointment/Show";
+import Confirm from "components/Appointment/Confirm";
+import Status from "components/Appointment/Status";
 
 storiesOf("Button", module)
   .addParameters({
@@ -139,3 +142,6 @@ storiesOf("Appointment", module)
 .add("Appointment with Time", () => <Appointment time="12pm" />)
 .add("Header", () => <Header time="12pm" />)
 .add("Empty", () => <Empty onAdd={action("onAdd")} />)
+.add("Show", () => <Show student="Carly Rae" interviewer= {{name:'Joseph Maroon'}} onEdit={action("onEdit")} onDelete={action("onDelete")} />)
+.add("Confirm", () => <Confirm message="Delete the appointment?" onConfirm={action("onConfirm")} onCancel={action("onCancel")} />)
+.add("Status", () => <Status message="Deleting" />)
