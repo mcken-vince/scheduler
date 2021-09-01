d4fb48c5466d84fe0b646be5ec0664ef481f840d
mcken-vince <mcken.vince@gmail.com>
2021-09-01 14:21:04 -0600

    create Appointment component

---
+++ src/components/Appointment/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './styles.scss';
+
+const Appointment = () => {
+  return (
+    <article className="appointment"></article>
+  );
+};
+
+export default Appointment;
