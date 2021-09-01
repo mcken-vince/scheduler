93fe76e2bf4ef57716a0c54e4cf5a2cd9374c0ab
mcken-vince <mcken.vince@gmail.com>
2021-09-01 16:35:33 -0600

    change Emptycomponent className to get scss styles working

--- src/components/Appointment/Empty.js
+++ src/components/Appointment/Empty.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Empty = (props) => {
   return (
-    <main className="appointment_add">
+    <main className="appointment__add">
       <img
         className="appointment__add-button"
         src="images/add.png"
