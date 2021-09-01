23fa3ba2bee87b536787709084a5b9219412000f
mcken-vince <mcken.vince@gmail.com>
2021-09-01 16:39:52 -0600

    clean up code

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -3,13 +3,14 @@ import './styles.scss';
 import Header from './Header';
 import Show from './Show';
 import Empty from './Empty';
+import { action } from '@storybook/addon-actions/dist/preview';
 
 
 const Appointment = (props) => {
   return (
     <article className="appointment">
       <Header time={props.time} />
-      {props.interview ? <Show student={props.interview.student} interviewer={props.interview.interviewer}/> : <Empty />}
+      {props.interview ? <Show student={props.interview.student} interviewer={props.interview.interviewer} /> : <Empty />}
       
     </article>
   );
