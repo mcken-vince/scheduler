9aca08ceb3b9c7f52f77a57eca43eb8c19509043
mcken-vince <mcken.vince@gmail.com>
2021-09-01 16:25:10 -0600

    modify Appointment component to render either <Show/> or <Empty/> depending on whether an interview is booked or not

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -2,14 +2,14 @@ import React from 'react';
 import './styles.scss';
 import Header from './Header';
 import Show from './Show';
-import Confirm from './Confirm';
 import Empty from './Empty';
-import Status from './Status';
-import Error from './Error';
+
 
 const Appointment = (props) => {
   return (
     <article className="appointment">
+      <Header time={props.time} />
+      {props.interview ? <Show student={props.interview.student} interviewer={props.interview.interviewer}/> : <Empty />}
       
     </article>
   );
--- stories/index.js
+++ stories/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Fragment } from "react";
 
 import { storiesOf } from "@storybook/react";
 import { action } from "@storybook/addon-actions";
@@ -151,3 +151,19 @@ storiesOf("Appointment", module)
 .add("Error", () => <Error message="Could not delete appointment" onClose={action("onClose")} />)
 .add("Form - Edit", () => <Form name="Carly Rae" interviewers={interviewers} interviewer={1} onSave={action("onSave")} onCancel={action("onCancel")} />)
 .add("Form - Create", () => <Form interviewers={interviewers} onSave={action("onSave")} onCancel={action("onCancel")}/>)
+.add("Appointment Empty", () => (
+  <Fragment>
+    <Appointment id={1} time="12pm" />
+    <Appointment id="last" time="1pm" />
+  </Fragment>
+))
+.add("Appointment Booked", () => (
+  <Fragment>
+    <Appointment
+      id={1}
+      time="12pm"
+      interview={{ student: "Lydia Miller-Jones", interviewer }}
+    />
+    <Appointment id="last" time="1pm" />
+  </Fragment>
+))
