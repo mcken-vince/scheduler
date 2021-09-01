f25eeabd5a9295cbef6b4775a3cb64481afc44fd
mcken-vince <mcken.vince@gmail.com>
2021-09-01 15:20:11 -0600

    create Appointment Confirm component

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import './styles.scss';
 import Header from './Header';
+import Show from './Show';
 
 const Appointment = (props) => {
   return (
     <article className="appointment">
       <Header time={props.time} />
+      <Show 
+        student={props.student} 
+        interviewer={props.interviewer} 
+        onEdit={()=>{}}
+        onDelete={()=>{}}  
+      />
     </article>
   );
 };
