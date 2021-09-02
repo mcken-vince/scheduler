cc9f533f2c7ed3c22ce6388ddfde64d14cda2b86
mcken-vince <mcken.vince@gmail.com>
2021-09-02 16:25:08 -0600

    create back() functionality in useVisualMode hook, pass 3 tests

--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -2,7 +2,24 @@ import { useState } from 'react';
 
 const useVisualMode = (initial) => {
   const [ mode, setMode ] = useState(initial);
-  return { mode };
+  const [ history, setHistory ] = useState([initial]);
+
+  const transition = (val) => {
+    setHistory([...history, val]);
+    setMode(val)
+  };
+
+  const back = () => {
+    // make copy of history and remove last item, 
+    // then set history to value of copy
+    // and set mode to the last item of history
+    const historyCopy = [...history];
+    historyCopy.pop();
+    setHistory(historyCopy);
+    setMode(history[history.length - 2]);
+  };
+
+  return { mode, transition, back };
 };
 
-export default  useVisualMode ;
+export default useVisualMode ;
