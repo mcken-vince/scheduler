9bfb6cbced52b8e00b7243172acbf7b36617db8a
mcken-vince <mcken.vince@gmail.com>
2021-09-02 13:03:09 -0600

    import axios and get days data from api, remove hardcoded days data

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,7 +1,8 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import DayList from "./DayList";
 import "components/Application.scss";
 import Appointment from "components/Appointment";
+import axios from 'axios';
 
 const appointments = [
   {
@@ -55,24 +56,12 @@ const appointments = [
 
 export default function Application(props) {
   const [ day, setDay ] = useState("Monday");
+  const [ days, setDays ] = useState([]);
 
-  const days = [
-    {
-      id: 1,
-      name: "Monday",
-      spots: 2,
-    },
-    {
-      id: 2,
-      name: "Tuesday",
-      spots: 5,
-    },
-    {
-      id: 3,
-      name: "Wednesday",
-      spots: 0,
-    },
-  ];
+  useEffect(() => {
+    axios.get('http://localhost:8001/api/days')
+    .then(response => setDays(response.data))
+  }, []);
 
   return (
     <main className="layout">
