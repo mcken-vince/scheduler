129066555ac764498879f46f8743d002e32c7701
mcken-vince <mcken.vince@gmail.com>
2021-09-02 15:39:51 -0600

    implement getInterview in Application, to send interview object as props to Appointment component

--- src/components/Application.js
+++ src/components/Application.js
@@ -3,14 +3,15 @@ import DayList from "./DayList";
 import "components/Application.scss";
 import Appointment from "components/Appointment";
 import axios from 'axios';
-import getAppointmentsForDay from "helpers/selectors";
+import { getAppointmentsForDay, getInterview } from "helpers/selectors";
 
 
 export default function Application(props) {
   const [ state, setState ] = useState({
     day: "Monday",
     days: [],
-    appointments: {}
+    appointments: {},
+    interviewers: {}
   });
 
   
@@ -27,7 +28,7 @@ export default function Application(props) {
     ])
     .then(all => {
       console.log(all);
-      setState(prev => ({...prev, days: all[0].data, appointments: all[1].data}));
+      setState(prev => ({ ...prev, days: all[0].data, appointments: all[1].data, interviewers: all[2].data }));
     })
   }, []);
 
@@ -54,7 +55,16 @@ export default function Application(props) {
         />
       </section>
       <section className="schedule">
-        {dailyAppointments.map(appointment => <Appointment key={appointment.id} {...appointment}/>)}
+        {dailyAppointments.map(appointment => {
+          const interview = getInterview(state, appointment.interview);
+          
+          return (<Appointment 
+            key={appointment.id} 
+            id={appointment.id}
+            time={appointment.time}
+            interview={interview}
+          />)
+      })}
       </section>
     </main>
   );
