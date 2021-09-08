9c865c36058538d192ec0faf1e482cef282c7d26
mcken-vince <mcken.vince@gmail.com>
2021-09-08 08:55:11 -0600

    add missing last-appointment 5pm slot, uncomment css so that only header displaying time is showing

--- src/components/Application.js
+++ src/components/Application.js
@@ -47,6 +47,7 @@ export default function Application(props) {
       </section>
       <section className="schedule">
         {appointments}
+        <Appointment key="last" time="5pm" />
       </section>
     </main>
   );
