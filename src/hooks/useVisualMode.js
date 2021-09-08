974867620bb0e5b6dfe248882c8bccc8208f3586
mcken-vince <mcken.vince@gmail.com>
2021-09-08 17:15:07 -0600

    Write comments and documentation for functions in useVisualMode hook

--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -1,9 +1,19 @@
 import { useState } from 'react';
 
+/**
+ * Controls the state given to each Appointment component, determines what the component will display
+ * @param {string} initial mode to initialize component with (SHOW, EMPTY)
+ * @returns {object} containing mode value, and functions with which to move back and forth between modes
+ */
 const useVisualMode = (initial) => {
   const [ mode, setMode ] = useState(initial);
   const [ history, setHistory ] = useState([initial]);
 
+  /**
+   * Used by Appointment components to change mode, updates both history and mode state
+   * @param {string} val new mode being assigned
+   * @param {boolean} replace whether to replace last item of history or not
+   */
   const transition = (val, replace=false) => {
     const updatedHistory = [...history];
     if (replace) {
@@ -15,10 +25,11 @@ const useVisualMode = (initial) => {
     setMode(val)
   };
 
+  /**
+   * Used by Appointment components to move back one mode in history, updates both history and mode state
+   */
   const back = () => {
-    // make copy of history minus last item 
-    // then set history to value of copy
-    // and set mode to the last item of history
+
     if (history.length > 1) {
       setHistory(history.slice(0, history.length - 1));
       setMode(history[history.length - 2]);
