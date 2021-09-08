5640108c5b5da8095d4878546ddc6f5aea321fc4
mcken-vince <mcken.vince@gmail.com>
2021-09-07 20:58:44 -0600

    implement appointment editing test

--- src/components/__tests__/Application.test.js
+++ src/components/__tests__/Application.test.js
@@ -56,14 +56,34 @@ describe("Application", () => {
 
     await waitForElement(() => getByAltText(appointment, "Add"));
 
-    const day = getAllByTestId(container, 'day').find(day =>
+    const day = getAllByTestId(container, "day").find(day =>
       queryByText(day, "Monday")
     );
     expect(getByText(day, "2 spots remaining")).toBeInTheDocument();
   });
 
-  it("loads data, edits an interview and keeps the spots remaining for Monday the same", () => {
+  it("loads data, edits an interview and keeps the spots remaining for Monday the same", async () => {
+    const { debug, container } = render(<Application />);
+    await waitForElement(() => getByText(container, "Archie Cohen"));
+    const appointment = getAllByTestId(container, "appointment").find(
+      appointment => queryByText(appointment, "Archie Cohen")
+    );
 
+    fireEvent.click(getByAltText(appointment, "Edit"));
+    fireEvent.click(getByAltText(appointment, "Sylvia Palmer"));
+    const input = getByPlaceholderText(appointment, "Enter Student Name");
+    fireEvent.change(input, { target: { value: "Bobby McDylan" } });
+    fireEvent.click(getByText(appointment, "Save"));
+
+    expect(getByText(appointment, "Saving")).toBeInTheDocument();
+
+    await waitForElement(() => getByText(appointment, "Bobby McDylan"));
+    expect(getByText(appointment, "Sylvia Palmer")).toBeInTheDocument();
+
+    const day = getAllByTestId(container, "day").find(day =>
+      queryByText(day, "Monday")
+    );
+    expect(getByText(day, "1 spot remaining")).toBeInTheDocument();
   });
 
   it("shows the save error when failing to save an appointment", () => {
